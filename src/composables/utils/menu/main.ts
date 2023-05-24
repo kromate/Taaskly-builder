5f129dddbf2d6d5d6bd19ff42ba0ba916e372561
Drex <davidokunoye003@gmail.com>
2023-05-24 20:04:55 +0100

    Added to the navbar

--- src/composables/utils/menu/main.ts
+++ src/composables/utils/menu/main.ts
@@ -6,72 +6,91 @@ import help from '~~/src/assets/icons/src/help.vue'
 import sites from '~~/src/assets/icons/src/sites.vue'
 
 export const mainRoutes = [
-	{
-		icon: sites,
-		name: 'Sites',
-		route: '/main/home'
-	},
-	{
-		icon: shop,
-		name: 'Themes',
-		route: '/main/themes'
-	},
-	{
-		icon: service,
-		name: 'Domains',
-		route: '/main/domains'
-	},
-	{
-		icon: profile,
-		name: 'Account',
-		route: '/main/account'
-	},
-	{
-		icon: help,
-		name: 'Support',
-		route: '/main/support'
-	}
-
+  {
+    icon: sites,
+    name: 'Sites',
+    route: '/main/home'
+  },
+  {
+    icon: shop,
+    name: 'Themes',
+    route: '/main/themes'
+  },
+  {
+    icon: service,
+    name: 'Domains',
+    route: '/main/domains'
+  },
+  {
+    icon: profile,
+    name: 'Account',
+    route: '/main/account'
+  },
+  {
+    icon: help,
+    name: 'Support',
+    route: '/main/support'
+  }
 ] as any
 
 export const mainTopbarName = {
-	'main-tasks-id': {
-		name: 'Sites',
-		can_go_back: true,
-		back_routes: '/main/home'
-	},
-	'main-home': {
-		name: 'Sites',
-		can_go_back: false
-	},
-	'main-themes': {
-		name: 'Themes',
-		can_go_back: false
-	},
-	'main-domains': {
-		name: 'Domains',
-		can_go_back: false
-	},
-	'main-account': {
-		name: 'Accounts',
-		can_go_back: false
-	},
-	'main-support': {
-		name: 'Support',
-		can_go_back: false
-	}
+  'main-tasks-id': {
+    name: 'Sites',
+    can_go_back: true,
+    back_routes: '/main/home',
+    button_title: 'Create site'
+  },
+  'main-home': {
+    name: 'Sites',
+    can_go_back: false,
+	button_title: 'Create site'
+  },
+  'main-themes': {
+    name: 'Themes',
+    can_go_back: false,
+	button_title: 'Create Theme'
+
+  },
+  'main-domains': {
+    name: 'Domains',
+    can_go_back: false,
+	button_title: 'Create Domain'
+
+  },
+  'main-account': {
+    name: 'Accounts',
+    can_go_back: false,
+	button_title: 'Create site'
+
+  },
+  'main-support': {
+    name: 'Support',
+    can_go_back: false,
+	button_title: 'Create site'
+
+  }
 }
 
 export const mainTopbarNameFunction = (name: string) => {
-	if (name in mainTopbarName) {
-		if (mainTopbarName[name].custom_name) {
-			mainTopbarName[name].name = formatName(useRoute().params.id as string)
-		}
-		return mainTopbarName[name]
-	}
-	return { name }
+  if (name in mainTopbarName) {
+    if (mainTopbarName[name].custom_name) {
+      mainTopbarName[name].name = formatName(useRoute().params.id as string)
+    }
+    return mainTopbarName[name]
+  }
+  return { name }
+}
+
+export const mainTopBarButtonFunction = (name: string) => {
+  if (name in mainTopbarName) {
+    if (mainTopbarName[name].custom_name) {
+      mainTopbarName[name].name = formatName(useRoute().params.id as string)
+    }
+    return mainTopbarName[name]
+  }
+  return { name }
 }
 
 const formatName = (name: string) => {
-	if (name) return name.replaceAll('-', ' ')
+  if (name) return name.replaceAll('-', ' ')
 }
