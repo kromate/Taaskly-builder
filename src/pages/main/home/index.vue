93b5324ca5c7882b1b137dee90e2238b6d6b8c1e
kromate <akpananthony33@gmail.com>
2023-05-27 00:03:27 +0100

    .

--- src/pages/main/home/index.vue
+++ src/pages/main/home/index.vue
@@ -4,28 +4,29 @@
       welcome back, {{ username }}
     </h1>
 
-    <article
-      class="border border-primary rounded-md w-[200px] h-[200px] shadow-xl py-6 px-3 flex flex-col justify-between"
-    >
-      <div class="flex flex-col gap-1">
-        <h3 class="font-bold text-lg">
-          Name of site
-        </h3>
-        <span class="text-xs">updated on 23 may, 2023</span>
-      </div>
+    <section class="grid grid-cols-3">
+      <article class="border border-primary rounded-md w-full h-[200px] shadow-xl  p-4 flex flex-col justify-between relative">
+        <div class="flex flex-col gap-1">
+          <h3 class="font-bold text-xl pr-6">
+            Let's get started on create your next awesome website
+          </h3>
+          <span>it's easier than you think</span>
+          <shine class="absolute right-4 top-10 text-primary opacity-75" />
+        </div>
 
-      <div class="flex gap-4 items-center">
-        <button><Edit /> <span class="sr-only">edit</span></button>
-        <button><Preview /><span class="sr-only">preview</span></button>
-      </div>
-    </article>
+        <button class="modal-btn">
+          Create Site
+          <Add />
+        </button>
+      </article>
+    </section>
   </section>
 </template>
 
 <script setup>
 import { useUser } from '@/composables/auth'
-import Edit from '@/assets/icons/src/edit.vue'
-import Preview from '@/assets/icons/src/preview.vue'
+import Add from '@/assets/icons/src/add.vue'
+import Shine from '@/assets/icons/src/shine.vue'
 definePageMeta({
   layout: 'default',
   middleware: ['has-no-profile']
