3286c206755d301cf861ae940c7450ec0bb1b4e7
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-05-29 12:54:57 +0100

    added iframe

--- src/pages/main/editor/index.vue
+++ src/pages/main/editor/index.vue
@@ -1,19 +1,21 @@
 <template>
     <main>
-        <div class="flex h-screen">
-                <div class="flex-1">
+        <div class="grid grid-cols-2 h-screen">
+                <div>
                     <h3>HTML</h3>
                     <HTMLEditor />
                 </div>
-            <div>display</div>
+            <div><iframe ref="iframe" class="w-full h-full" sandbox="allow-scripts allow-same-origin" /></div>
         </div>
     </main>
 </template>
 
-<script setup>
-// import { ref } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import HTMLEditor from '@/components/editor/HTMLEditor.vue'
 definePageMeta({
     layout: 'default'
 })
+
+const iframe = ref<HTMLIFrameElement>()
 </script>
