5f6e663c5388d4f7fbaca3734b5ff250ac2ec0d5
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-05-29 12:30:08 +0100

    created the editor

---
+++ src/components/editor/HTMLEditor.vue
@@ -0,0 +1,14 @@
+<template>
+    <div ref="container" class="w-full h-full" />
+</template>
+
+<script setup lang="ts">
+    import * as monaco from 'monaco-editor'
+    import { ref, onMounted } from 'vue'
+
+    const container = ref<HTMLDivElement|null>(null)
+
+    onMounted(() => {
+        const html = monaco.editor.create(container.value!, { language: 'html', theme: 'vs-dark', lineHeight: 2 })
+    })
+</script>
---
+++ src/pages/main/editor/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        <div class="flex h-screen">
+                <div class="flex-1">
+                    <h3>HTML</h3>
+                    <HTMLEditor />
+                </div>
+            <div>display</div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+import HTMLEditor from '@/components/editor/HTMLEditor.vue'
+definePageMeta({
+    layout: 'default'
+})
+</script>
