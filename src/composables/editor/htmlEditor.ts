6e26a108346d35e56147e8edb6b5e59df093e80e
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-05-29 19:41:59 +0100

    able to display html content from local strorage

--- src/composables/editor/htmlEditor.ts
+++ src/composables/editor/htmlEditor.ts
@@ -1,6 +1,10 @@
 import * as monaco from 'monaco-editor'
-
+import { ref } from 'vue'
+import { useStorage, useDebounceFn } from '@vueuse/core'
+import { initialEditorValue } from './initials'
 export let htmlEditor: monaco.editor.IStandaloneCodeEditor
+
+const editorValue = useStorage('editor-value', initialEditorValue)
 
 export const mountHTMLEditor = (container: HTMLDivElement) => {
     htmlEditor = monaco.editor.create(container, {
@@ -8,6 +12,7 @@ export const mountHTMLEditor = (container: HTMLDivElement) => {
         theme: 'vs-dark',
         lineHeight: 2
     })
+    htmlEditor.setValue(editorValue.value.html)
 }
 
 export const unMountHTMLEditor = () => {
