e9f0d6314fe463c8af637c1caa3d7dd249bb6ec7
Drex <davidokunoye003@gmail.com>
2023-07-05 12:45:54 +0100

    fixed routes

---
+++ src/components/modals/CreateComponent.vue
@@ -0,0 +1,28 @@
+<template>
+	<Modal modal="$atts.modal" title="Create a new Component" :is-full-height="false">
+		<form class="auth-form" @submit.prevent="create">
+			<div class="field">
+				<label for="name">Component Name</label>
+				<input id="name" v-model="createComponentForm.name.value" placeholder="My Component" type="text" class="input-field" required minlength="3">
+			</div>
+			<div class="field">
+				<label for="desc">Component Description</label>
+				<textarea id="desc" v-model="createComponentForm.desc.value" placeholder="This is My Component" type="text" rows="4" class="input-field h-24 pt-3" required minlength="3" />
+			</div>
+			<button class="btn-primary w-full mt-2" type="submit">
+				<span v-if="!loading">Create Component</span>
+				<Spinner v-else />
+			</button>
+		</form>
+	</Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '@/components/core/modal/Modal.vue'
+import { useCreateComponent } from '@/composables/components/create'
+
+const { create, createComponentForm, loading } = useCreateComponent()
+
+</script>
+
+<style></style>
